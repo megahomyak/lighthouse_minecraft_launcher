c21e7d16fc588092217d599d208c07772d9d1d38
megahomyak <g.megahomyak@gmail.com>
2024-10-20 04:59:42 +0500

    "no lwjgl in java.library.path: version/libraries"...

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 class_path = ["version/client.jar"]
 for file_name in os.listdir("version/libraries"):
     class_path.append(f"version/libraries/{file_name}")
-command = f"java -cp {':'.join(class_path)} net.minecraft.client.Minecraft"
+command = f"java -cp {':'.join(class_path)} -Djava.library.path=version/libraries net.minecraft.client.Minecraft"
 print(command)
 print()
 os.system(command)
