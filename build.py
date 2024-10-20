168fa6a44efda208fbc19c8d4c53a11167ebcb57
megahomyak <g.megahomyak@gmail.com>
2024-10-21 00:53:01 +0500

    Made Minecraft run

--- build.py
+++ build.py
@@ -2,15 +2,19 @@ import json
 import requests
 import os
 
-def download(directory, url):
-    os.system(f"mkdir -p {directory}")
-    os.system(f"wget {url} -O {directory}/$(basename {url})")
+try: os.system("rm -r version")
+except: pass
+
+def download(file_path, url):
+    os.system(f"mkdir -p $(dirname {file_path})")
+    os.system(f"wget {url} -O {file_path}")
 
 version = requests.get("https://piston-meta.mojang.com/v1/packages/924a2dcd8bdc31f8e9d36229811c298b3537bbc7/1.5.2.json").json()
-download("version", version["downloads"]["client"]["url"])
-for library in version["libraries"]:
-    try: download("version/libraries", library["downloads"]["artifact"]["url"])
+download("version/client.jar", version["downloads"]["client"]["url"])
+for index, library in enumerate(version["libraries"]):
+    try: download(f"version/libraries/{index}.jar", library["downloads"]["artifact"]["url"])
     except KeyError: pass
 
-    try: download("version/libraries", library["downloads"]["classifiers"]["natives-linux"]["url"])
+    try: download(f"version/libraries/natives/{index}.jar", library["downloads"]["classifiers"]["natives-linux"]["url"])
     except KeyError: pass
+    else: os.system(f"cd version/libraries/natives && unzip {index}.jar && rm {index}.jar && rm -r META-INF")
