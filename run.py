c90d3eaf5a53b92946da65909e88e989b329a724
megahomyak <g.megahomyak@gmail.com>
2024-10-20 18:55:24 +0500

    Trying to use realpath, failing

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 
-class_path = ["version/client.jar"]
+class_path = ["$(realpath version/client.jar)"]
 for file_name in os.listdir("version/libraries"):
-    class_path.append(f"version/libraries/{file_name}")
-command = f"java -cp {':'.join(class_path)} -Djava.library.path=version/libraries net.minecraft.client.Minecraft"
+    class_path.append(f"$(realpath version/libraries/{file_name})")
+command = f"java -cp {':'.join(class_path)} -Djava.library.path=$(realpath version/libraries) net.minecraft.client.Minecraft"
 print(command)
 print()
 os.system(command)
