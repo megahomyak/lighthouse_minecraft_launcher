d504b4256ab3e78b4b10396bd9a145f77909e3ba
megahomyak <g.megahomyak@gmail.com>
2024-10-21 01:28:34 +0500

    Moved the state out

--- build.py
+++ build.py
@@ -9,12 +9,18 @@ def download(file_path, url):
     os.system(f"mkdir -p $(dirname {file_path})")
     os.system(f"wget {url} -O {file_path}")
 
+VERSION_DIR = "version"
+LIBRARIES_DIR = f"{VERSION_DIR}/libraries"
+NATIVES_DIR = f"{LIBRARIES_DIR}/natives"
+
 version = requests.get("https://piston-meta.mojang.com/v1/packages/924a2dcd8bdc31f8e9d36229811c298b3537bbc7/1.5.2.json").json()
-download("version/client.jar", version["downloads"]["client"]["url"])
+download("{VERSION_DIR}/client.jar", version["downloads"]["client"]["url"])
 for index, library in enumerate(version["libraries"]):
-    try: download(f"version/libraries/{index}.jar", library["downloads"]["artifact"]["url"])
+    try: download(f"{LIBRARIES_DIR}/{index}.jar", library["downloads"]["artifact"]["url"])
     except KeyError: pass
 
-    try: download(f"version/libraries/natives/{index}.jar", library["downloads"]["classifiers"]["natives-linux"]["url"])
+    try: download(f"{NATIVES_DIR}/{index}.jar", library["downloads"]["classifiers"]["natives-linux"]["url"])
     except KeyError: pass
-    else: os.system(f"cd version/libraries/natives && unzip {index}.jar && rm {index}.jar && rm -r META-INF")
+    else: os.system(f"cd {NATIVES_DIR} && unzip {index}.jar && rm {index}.jar && rm -r META-INF")
+
+os.system("mkdir {VERSION_DIR}/state")
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 class_path = ["version/client.jar"]
 for file_name in os.listdir("version/libraries"):
     class_path.append(f"version/libraries/{file_name}")
-command = f"/usr/lib/jvm/java-8-openjdk-amd64/bin/java -cp {':'.join(class_path)} -Djava.library.path=version/libraries/natives net.minecraft.launchwrapper.Launch"
+command = f"/usr/lib/jvm/java-8-openjdk-amd64/bin/java -cp {':'.join(class_path)} -Djava.library.path=version/libraries/natives net.minecraft.launchwrapper.Launch --gameDir version/state"
 print(command)
 print()
 os.system(command)
