724715f4a2a649de23d24d8b8b0f6da7d9f8b393
megahomyak <g.megahomyak@gmail.com>
2024-10-21 20:45:41 +0500

    Better file names

--- install.py
+++ install.py
@@ -42,7 +42,7 @@ def download_file(file_path, url):
 library_paths = []
 
 version = requests.get(version_url).json()
-# download_file("client.jar", version["downloads"]["client"]["url"])
+download_file("client.jar", version["downloads"]["client"]["url"])
 for index, library in enumerate(version["libraries"]):
     if "rules" in library:
         allowed = False
@@ -60,10 +60,11 @@ for index, library in enumerate(version["libraries"]):
     except KeyError:
         pass
     else:
-        continue
-        artifact_path = library["downloads"]["artifact"]["path"]
-        download_file(f"libraries/{artifact_path}", artifact_url)
-        library_paths.append(artifact_path)
+        provided_artifact_path = library["downloads"]["artifact"]["path"]
+        artifact_name = os.path.basename(provided_artifact_path)
+        actual_artifact_path = f"libraries/{index}-{artifact_name}"
+        download_file(actual_artifact_path, artifact_url)
+        library_paths.append(actual_artifact_path)
 
     try:
         natives_url = library["downloads"]["classifiers"]["natives-linux"]["url"]
@@ -71,7 +72,7 @@ for index, library in enumerate(version["libraries"]):
         pass
     else:
         download_file(f"libraries/natives/natives.jar", natives_url)
-        os.system(f"cd libraries/natives && unzip -o natives.jar && rm natives.jar")
+        os.system(f"cd libraries/natives && unzip -o natives.jar && rm natives.jar && rm -rf META-INF")
 
 ### Preparing the runner script
 
@@ -79,10 +80,7 @@ java_version = version["javaVersion"]["majorVersion"]
 
 java_path = subprocess.run(["sh", "-c", f"update-alternatives --list java | grep java-{java_version}-"], capture_output=True).stdout.decode("utf-8").strip() or f"JAVA_{java_version}_NOT_FOUND"
 
-class_path = ["client.jar"] + [
-    f"libraries/{library_path}"
-    for library_path in library_paths
-]
+class_path = ["client.jar"] + library_paths
 
 with open("run.sh", "w") as f:
     f.write("#!/usr/bin/sh\n")
