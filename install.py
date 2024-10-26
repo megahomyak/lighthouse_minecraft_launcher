1591644902b020026e9d2da9be02a17e6b000b87
megahomyak <g.megahomyak@gmail.com>
2024-10-26 22:41:05 +0500

    Checking hash sums, better logging, detached `run`

--- install.py
+++ install.py
@@ -9,11 +9,10 @@ import subprocess
 import stat
 import shutil
 import pathlib
+import hashlib
 
 LIGHTHOUSE_VERSION_ID = "megahomyak-1"
 LIGHTHOUSE_CONFIG_NAME = "lighthouse.config.json"
-
-TEN_MEGABYTES_IN_BYTES = 1 * 1024 * 1024 * 10
 
 # Stolen from portablemc
 current_os_name = {
@@ -47,6 +46,8 @@ def download_json(url):
     return requests.get(url).json()
 
 def get_versions_list():
+    #####
+    print("Getting the versions list")
     return download_json("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json")
 
 def make_executable(file_path):
@@ -57,14 +58,28 @@ def main():
     action = sys.argv[1]
     if action == "list":
         list_()
-    elif action == "install":
+    elif action == "ensure":
         version_id = sys.argv[2]
-        install(version_id)
+        ensure(version_id)
     elif action == "run":
         version_id = sys.argv[2]
         run(version_id)
     else:
         raise Exception(f"Invalid action name \"{action}\"")
+
+def get_sha1(path):
+    with open(path, "rb") as f:
+        return hashlib.sha1(f.read())
+
+def download(url, path, expected_sha1_in_hex):
+    print(f"Checking {path}... ", end="", flush=True)
+    try:
+        assert get_sha1(path).hexdigest() == expected_sha1_in_hex
+    except:
+        print("DOWNLOADING")
+        urllib.request.urlretrieve(url, path)
+    else:
+        print("EXISTS")
 
 def list_():
     versions_list = get_versions_list()
@@ -83,11 +98,12 @@ def run(version_id):
     if config_lighthouse_version_id == LIGHTHOUSE_VERSION_ID:
         arguments = config["run_arguments"]
         java_binary_path = config["java_binary_path"]
-        subprocess.run([java_binary_path, *arguments])
+        print(f"Starting up {version_id} in a detached process")
+        subprocess.Popen([java_binary_path, *arguments], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     else:
         raise Exception(f"Bad config version: expected {LIGHTHOUSE_VERSION_ID}, got {config_lighthouse_version_id}")
 
-def install(version_id):
+def ensure(version_id):
     versions_list = get_versions_list()["versions"]
     for version in versions_list:
         if version["id"] == version_id:
@@ -96,60 +112,70 @@ def install(version_id):
     else:
         raise Exception(f"Can't find version {version_id}")
 
+    #####
+    print("Writing the Lighthouse warning file")
+
     with open("WARNING_READ_ME.txt", "w") as f:
-        f.write("This directory is being used for storing Minecraft instances of the Lighthouse Minecraft launcher. Modifying this directory manually (that is, not through Lighthouse) might prevent Lighthouse from functioning well here. All file-dependant calls to Lighthouse should be done from this directory.")
+        f.write("This directory is used for the Lighthouse Minecraft launcher. Please, only modify the state of Minecraft version instances. Every other modification may break Lighthouse operations.")
+
+    #####
+    print("Getting the version JSON")
 
     version = requests.get(version_url).json()
 
     #####
-    print("Installing the runtime")
+    print("Checking the runtime")
 
     java_runtime_name = version["javaVersion"]["component"]
     java_runtime_path = os.path.join("runtimes", java_runtime_name)
-    try:
-        os.makedirs(java_runtime_path, exist_ok=False)
-    except FileExistsError:
-        pass
-    else:
-        runtimes = requests.get("https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json").json()
-        files_url = runtimes[minecraft_jvm_os][java_runtime_name][0]["manifest"]["url"]
-        files = requests.get(files_url).json()["files"]
-        for path, file in files.items():
-            path = os.path.join(java_runtime_path, path)
-            os.makedirs(os.path.dirname(path), exist_ok=True)
-            if file["type"] == "directory":
-                pass # Already created using os.makedirs above
-            elif file["type"] == "file":
-                file_url = file["downloads"]["raw"]["url"]
-                print(f"Downloading {file_url}")
-                urllib.request.urlretrieve(file_url, path)
-                if file["executable"]:
-                    make_executable(path)
-            elif file["type"] == "link":
-                os.symlink(file["target"], path)
-            else:
-                raise Exception(f"Unrecognized runtime file type: \"{file['type']}\" at \"{path}\"")
+    os.makedirs(java_runtime_path, exist_ok=True)
+    #####
+    print("Getting the runtimes list")
+
+    runtimes = requests.get("https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json").json()
+    files_url = runtimes[minecraft_jvm_os][java_runtime_name][0]["manifest"]["url"]
+    #####
+    print("Getting the runtime files")
+
+    files = requests.get(files_url).json()["files"]
+    for path, file in files.items():
+        path = os.path.join(java_runtime_path, path)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        if file["type"] == "directory":
+            pass # Already created using os.makedirs above
+        elif file["type"] == "file":
+            raw = file["downloads"]["raw"]
+            download(raw["url"], path, raw["sha1"])
+            if file["executable"]:
+                make_executable(path)
+        elif file["type"] == "link":
+            try: os.symlink(file["target"], path)
+            except: pass
+        else:
+            raise Exception(f"Unrecognized runtime file type: \"{file['type']}\" at \"{path}\"")
 
     #####
-    print("Installing the client")
+    print("Checking the client")
 
-    shutil.rmtree(version_id, ignore_errors=True)
-    os.mkdir(version_id)
+    try: os.mkdir(version_id)
+    except: pass
     os.chdir(version_id)
-    os.mkdir("libraries")
-    os.mkdir(os.path.join("libraries", "natives"))
-    os.mkdir("state")
+    try: os.mkdir("libraries")
+    except: pass
+    try: os.mkdir(os.path.join("libraries", "natives"))
+    except: pass
+    try: os.mkdir("state")
+    except: pass
 
     library_paths = []
 
     NATIVES_DIR_PATH = os.path.join("libraries", "natives")
 
-    client_url = version["downloads"]["client"]["url"]
-    print(f"Downloading {client_url}")
-    urllib.request.urlretrieve(client_url, "client.jar")
+    client = version["downloads"]["client"]
+    download(client["url"], "client.jar", client["sha1"])
 
     #####
-    print("Installing the libraries")
+    print("Checking the libraries")
 
     for index, library in enumerate(version["libraries"]):
         if "rules" in library:
@@ -164,30 +190,27 @@ def install(version_id):
                 continue
 
         try:
-            artifact_url = library["downloads"]["artifact"]["url"]
+            artifact = library["downloads"]["artifact"]
         except KeyError:
             pass
         else:
-            provided_artifact_path = library["downloads"]["artifact"]["path"]
+            provided_artifact_path = artifact["path"]
             artifact_name = os.path.basename(provided_artifact_path)
             actual_artifact_path = os.path.join("libraries", f"{index}-{artifact_name}")
-            print(f"Downloading {artifact_url}")
-            urllib.request.urlretrieve(artifact_url, actual_artifact_path)
+            download(artifact["url"], actual_artifact_path, artifact["sha1"])
             library_paths.append(actual_artifact_path)
 
         try:
-            natives_url = library["downloads"]["classifiers"][f"natives-{current_os_name}"]["url"]
+            natives = library["downloads"]["classifiers"][f"natives-{current_os_name}"]
         except KeyError:
             pass
         else:
-            natives_jar_path = os.path.join(NATIVES_DIR_PATH, "natives.jar")
-            print(f"Downloading {natives_url}")
-            urllib.request.urlretrieve(natives_url, natives_jar_path)
-            with zipfile.ZipFile(natives_jar_path) as f:
+            download(natives["url"], "natives.jar", natives["sha1"])
+            with zipfile.ZipFile("natives.jar") as f:
                 for member in f.namelist():
                     if member != "META-INF":
                         f.extract(member, NATIVES_DIR_PATH)
-            os.remove(natives_jar_path)
+            os.remove("natives.jar")
 
     #####
     print("Writing the lighthouse data file")
